1483a54fc3a6b18a2ebed660684f68a7edce8ff2
Owen Brooks <owen.h.brooks@gmail.com>
2022-10-12 22:44:47 +1100

    Attempt to max filter spectrogram

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,40 @@ use sonogram::ColourGradient;
 use sonogram::ColourTheme;
 use sonogram::FrequencyScale;
 use sonogram::SpecOptionsBuilder;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::path::PathBuf;
 
 struct SpectogramImage {
     data: Vec<f32>,
-    width: usize,
-    height: usize,
+    pub width: usize,
+    pub height: usize,
+}
+
+impl SpectogramImage {
+    fn get_val(&self, x: usize, y: usize) -> f32 {
+        self.data[y * self.width + x]
+    }
+    fn get_square(&self, x: usize, y: usize, width: usize) -> Vec<f32> {
+        // returns values, centred on x,y coord
+        let mut data = vec![];
+        let min_x = if x < width / 2 { 0 } else { x - width / 2 };
+        let max_x = if x + width / 2 > self.width {self.width-1} else {x + width / 2 - 1};
+        let min_y = if y < width / 2 { 0 } else { y - width / 2 };
+        let max_y = if y + width / 2 > self.height {self.height-1} else {y + width / 2 - 1};
+        // println!("{} {} {} {}", min_x, max_x, min_y, max_y);
+        for i in min_x..=max_x {
+            for j in min_y..=max_y {
+                // data.push(self.get_val(i, j));
+            }
+        }
+        for i in 0..self.width {
+            for j in 0..self.height {
+                data.push(self.get_val(i, j));
+            }
+        }
+        data
+    }
 }
 
 #[derive(Parser, Debug)]
@@ -26,6 +53,7 @@ struct Args {
     input_wav: PathBuf,
 }
 
+// Reads wav file, creates a spectrogram, saves this to a png and to a buffer in memory.
 fn read_wav_to_png(input_wav: &PathBuf) -> Result<SpectogramImage, anyhow::Error> {
     // Read in the file info to determine length
     let mut inp_file = File::open(input_wav)?;
@@ -36,15 +64,16 @@ fn read_wav_to_png(input_wav: &PathBuf) -> Result<SpectogramImage, anyhow::Error
     // Compute the spectrogram giving the number of bins and the window overlap.
     let spec_builder = SpecOptionsBuilder::new(2048)
         .load_data_from_file(input_wav)
-        .unwrap();
+        .unwrap()
+        .normalise();
     let mut spectrograph = spec_builder.build().unwrap().compute();
 
     // Save the spectrogram to PNG.
     let time_scale = 1000;
     let spec_width = data_16.len() / time_scale;
-    let spec_height = 512;
+    let spec_height = 600;
     let png_file = std::path::Path::new("sonogram.png");
-    let mut gradient = ColourGradient::create(ColourTheme::Default);
+    let mut gradient = ColourGradient::create(ColourTheme::Rainbow);
     spectrograph.to_png(
         &png_file,
         FrequencyScale::Linear,
@@ -52,7 +81,8 @@ fn read_wav_to_png(input_wav: &PathBuf) -> Result<SpectogramImage, anyhow::Error
         spec_width,  // Width
         spec_height, // Height
     )?;
-    let spec_buffer = spectrograph.to_buffer(sonogram::FrequencyScale::Linear, spec_width, spec_height);
+    let spec_buffer =
+        spectrograph.to_buffer(sonogram::FrequencyScale::Linear, spec_width, spec_height);
 
     Ok(SpectogramImage {
         data: spec_buffer,
@@ -61,13 +91,49 @@ fn read_wav_to_png(input_wav: &PathBuf) -> Result<SpectogramImage, anyhow::Error
     })
 }
 
+fn max_filter(spec: &SpectogramImage, kernel_size: usize) -> Vec<f32> {
+    let mut filtered = vec![];
+    for y in 0..spec.height {
+        for x in 0..spec.width {
+            let square = spec.get_square(x, y, kernel_size);
+            let max = square.iter().max_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+            let max = spec.get_val(x, y);
+            // match max {
+            //     None => panic!("No maximum found"),
+            //     Some(max) => filtered.push(*max),
+            // }
+            filtered.push(max);
+        }
+    }
+    filtered
+}
+
 fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
 
     let _database_path = PathBuf::from(args.database);
     let spectrograph = read_wav_to_png(&args.input_wav)?;
 
-    println!("{:?}", &spectrograph.data);
+    let kernel_size = 5;
+
+    dbg!(spectrograph.width, spectrograph.height);
+
+    // let filtered = max_filter(&spectrograph, kernel_size);
+    let filtered = spectrograph.data.clone();
+
+    let spec_builder = SpecOptionsBuilder::new(2048).load_data_from_memory_f32(filtered, 11025);
+    let mut s = spec_builder.build().unwrap().compute();
+
+    // Save the spectrogram to PNG.
+    let png_file = std::path::Path::new("sonogram_max.png");
+    let mut gradient = ColourGradient::create(ColourTheme::Rainbow);
+    s.to_png(
+        &png_file,
+        FrequencyScale::Linear,
+        &mut gradient,
+        spectrograph.width,  // Width
+        spectrograph.height, // Height
+    )?;
 
     Ok(())
 }
